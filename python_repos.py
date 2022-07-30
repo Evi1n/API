1f5422138b5e6b50f3ace78d21380a1e77ae0477
Evi1n <evilnn.19@gmail.com>
2022-07-30 16:42:23 +0300

    python

--- python_repos.py
+++ python_repos.py
@@ -2,22 +2,21 @@ from urllib import response
 import requests
 
 
-# API çağrısına yanıt ver sakla
-
+# Respond and store API call
 url='https://api.github.com/search/repositories?q=langugage:python&sort=stars'
 headers= {'Accept': 'application/vnd.github.v3+json'}
 r= requests.get(url, headers=headers)
 print(f"Status code: {r.status_code}")
 
-# API yanıtını bir değişkende sakla.
+# Store the API response in a variable.
 response_dict= r.json()
 print(f"Total repositories: {response_dict['total_count']}")  # python depolarının toplam sayısını temsil eden total_count u ekrana yazdırdık.
 
-# depolar hakkındaki bilgileri incele
+# view information about repositories
 repo_dicts = response_dict['items']
 print(f"Repositories returned: {len(repo_dicts)}")
 
-print("\nSelected information about each repository:")  # en üst sıralardaki depoları listelemek.
+print("\nSelected information about each repository:")  # listing top-ranked repositories
 for repo_dict in repo_dicts:
     print(f"Name: {repo_dict['name']}")
     print(f"Owner: {repo_dict['owner']['login']}")
@@ -31,12 +30,9 @@ for repo_dict in repo_dicts:
 # ilk depoyu incele
 # repo_dict = repo_dicts[0]
 
-
 print(f"\nKeys: {len(repo_dict)}")
 #for key in sorted(repo_dict.keys()):
  #   print(key)
-
-
 
 # Sonuçları işle
 # print(response_dict.keys())
